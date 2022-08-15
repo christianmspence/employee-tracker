316f3680cbe0ba4c6f70ba9629a472bc93fb891d
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-08-15 14:49:18 -0400

    database and server set up

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (dept_name)
+VALUES
+('Executive'),
+('Finanace'),
+('Sales'),
+('Human Resources'),
+('Tech'),
+('Design');
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('./db/connection');
+// const apiRoutes = require('./routes/apiRoutes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Add after Express middleware
+// app.use('/api', apiRoutes);
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after DB connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
