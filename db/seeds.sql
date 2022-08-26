7304124ab1d4aeba096968829983e921bda2d186
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-08-26 13:49:00 -0400

    schemas and seeds set up

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO departments (dept_name)
+INSERT INTO departments (name)
 VALUES
 ('Executive'),
 ('Finanace'),
@@ -6,3 +6,29 @@ VALUES
 ('Human Resources'),
 ('Tech'),
 ('Design');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Graphic Designer', 50,000, 6),
+('Full-Stack Developer', 70,000, 5),
+('Payroll', 50,000, 4),
+('Sales Associate', 40,000, 3),
+('Accountant', 60,000, 2),
+('CFO', 80,000, 2),
+('CEO', 100,000, 1);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('James', 'Water', 1, 6),
+('Peter', 'Firth', 1, 6),
+('Grant', 'Foster', 2, 5),
+('John', 'Kim', 2, 5),
+('Matthew', 'Cheezem', 3, 4),
+('Linda', 'Spence', 3, 4),
+('Micah', 'Cheezem', 4, 3),
+('Eric', 'Smeal', 4, 3),
+('Savannah', 'Woodell', 4, 3),
+('Laura', 'Judd', 5, 2),
+('Luke', 'Stimmerman', 5, 2),
+('Clayton', 'Burklin', 6, 1),
+('Jesus', 'Christ', 7, NULL);
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./departmentRoutes'));
+router.use(require('./rolesRoutes'));
+
 
 
 module.exports = router;
