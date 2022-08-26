b13db6c969747af99c45ed670d188e15ebfe68b7
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-08-26 12:58:52 -0400

    working on department post route

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
 
 //Get parties
@@ -56,5 +57,29 @@ router.delete('/department/:id', (req, res) => {
         }
     });
 });
+
+// create department
+router.post('/department', ({ body }, res) => {
+    const errors = inputCheck(body, 'dept_name');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO departments (dept_name) VALUES (?)`;
+    const params = [body.dept_name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
 
 module.exports = router;
